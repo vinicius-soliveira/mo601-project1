2d9761d4653485fd164066ea2291b54c59ffb1d7
Vinicius Oliveira <vinicius.silva@lnls.br>
2023-03-21 17:17:06 -0300

    folder structure

---
+++ include/LogicCircuit.hpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#define AND(A, B) (A & B)
+#define OR(A, B) (A | B)
+#define NOT(A) (!A)
+#define NAND(A, B) !(A & B)
+#define NOR(A, B) !(A | B)
+#define XOR(A,B) (A ^ B)
+
+class LogicCircuit(){
+
+    enum logicGate_t{
+        AND,
+        OR,
+        NOT, 
+        NAND,
+        NOR,
+        XOR
+    };
+
+    enum terminal_t{
+        INPUT,
+        OUTPUT
+    };
+
+    private:
+
+        struct Signal{
+            char label;
+            terminal_t type;
+            bool value;
+        };
+
+        struct Gate{
+            logicGate_t type;
+            Signal* input;
+            Signal output;
+        };
+
+        Signal* signals;
+        Gate* gates;
+       
+
+    public:
+        LogicCircuit()
+        void assignOperation(bool* )
+        void setInputSignals(bool* );
+        bool* readOutputSignals();
+
+}
---
+++ include/Simulator.hpp
@@ -0,0 +1,14 @@
+#include <LogicCircuit>
+
+class Simulator {
+
+    private:
+        int time;
+        int delay;
+        
+
+    public:
+
+        void run();  
+
+};
